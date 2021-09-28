344d447e68d86f6ba11e20fca7101904b9dc1c3f
GregPinheiro <gpinheirojunior@gmail.com>
2021-09-28 14:15:57 -0300

    form for signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Home from './pages/Home'
 import {BrowserRouter as Router} from 'react-router-dom';
-
+import Signup from './components/Signup';
 
 function App() {
   return (
     <Router>  
       <Home />
+      {/* <Signup /> */}
     </Router>
   );
 }
