c7ebb21b6ef4940f1862e96f1e8cf07c83294aa5
Pati Bueno <patibuennodev@gmail.com>
2021-10-05 16:08:46 -0300

    Final Navbar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-import './App.css';
-import React, {useState, useCallback} from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Navbar from './components/Navbar'
-import CarouselW from './components/Carousel';
-import Manifesto from './components/Manifesto';
-import Footer from './components/Footer';
+import "./App.css";
+import React, { useState, useCallback } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Sidebar from "./components/Sidebar old";
+import NavbarPage from "./components/Navbar";
+import CarouselW from "./components/Carousel";
+import Manifesto from "./components/Manifesto";
+import Footer from "./components/Footer";
+import { AuthConext } from "./context/auth-context";
+import Switch from "react-bootstrap/esm/Switch";
+import FormSignUp from "./components/FormSignUp";
+import usersServices from "./services/users.services";
+import Brands from "./components/Brands";
 
-import { AuthConext } from './context/auth-context';
-import Switch from 'react-bootstrap/esm/Switch';
-import FormSignUp from './components/FormSignUp';
-import usersServices from './services/users.services';
-import Brands from './components/Brands';
-
-
-const handleSubmit = values => {
+const handleSubmit = (values) => {
   console.log(values);
-  usersServices.signup(values)
-  .then(response => {
-    alert('Usuario cadastrado com sucesso!');
-  })
-  .catch(e => {
-    e.response.data ? alert(e.response.data) : alert(e);
-    console.log(e);
-  })
-}
+  usersServices
+    .signup(values)
+    .then((response) => {
+      alert("Usuario cadastrado com sucesso!");
+    })
+    .catch((e) => {
+      e.response.data ? alert(e.response.data) : alert(e);
+      console.log(e);
+    });
+};
 // const handleSubmit = values => {alert(JSON.stringify(values))}
-const initialValues = {}
+const initialValues = {};
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-  const toggle = () =>{
-      setIsOpen(!isOpen);
-  }
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -44,23 +43,19 @@ function App() {
     setIsLoggedIn(false);
   }, []);
 
- 
-
   return (
-
-    <AuthConext.Provider value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}>
-     <Router>  
-     <FormSignUp handleSubmit={handleSubmit} initialValues={initialValues}/>
-          <Sidebar isOpen={isOpen} toggle={toggle} />
-          <Navbar toggle={toggle}  />
-          <CarouselW />
-          <Manifesto />
-          <Brands />
-          <Footer />
+    <AuthConext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <FormSignUp handleSubmit={handleSubmit} initialValues={initialValues} />
+        <NavbarPage />
+        <CarouselW />
+        <Manifesto />
+        <Brands />
+        <Footer />
       </Router>
     </AuthConext.Provider>
-)
-   
+  );
 }
 export default App;
-
